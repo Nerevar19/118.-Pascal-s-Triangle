9c0a71976fe31a9abaa612fb4ce0516366119599
Artem Kopytko <artem.kop19@gmail.com>
2023-07-26 16:21:07 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace _118._Pascal_s_Triangle
+{
+    internal class Program
+    {
+        static public IList<int> GenerateNext(IList<int> array)
+        {
+            var result = new int[array.Count() + 1];
+            result[0] = 1;
+            result[result.Length - 1] = 1;
+            for (int i = 1; i < result.Length - 1; i++)
+            {
+                result[i] = array[i - 1] + array[i];
+            }
+            return result;
+        }
+        static public IList<IList<int>> Generate(int numRows)
+        {
+            
+            var result = new List<IList<int>>();
+            if (numRows >= 1) result.Add(new int[] { 1 });
+
+
+                for (int i = 1; i < numRows; i++)
+            {
+               
+              int[] last =(int[])result.Last() ;
+              result.Add(GenerateNext(last)); 
+            }
+            return (IList<IList<int>>)result;
+            
+        }
+        static void Main(string[] args)
+        {
+            var pascalTriangle = Generate(30);
+            foreach (var outer in pascalTriangle)
+            {
+                foreach (var inner in outer)
+                {
+                    Console.Write(inner + " ");
+                }
+                Console.WriteLine();
+            }
+            /*var result = GenerateNext(new int[] { 1,1 });
+            foreach (var num in result)
+            {
+                Console.Write(num + " ");
+            }*/
+        }
+    }
+}
